b12aa1778b6f274c25239ddb3568f2c0a8279648
Himanshu Ladva <himanshu@webtheuniverse.com>
2022-08-25 10:55:26 +0530

    continue in crud app

--- src/app/post/view/view.component.ts
+++ src/app/post/view/view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-view',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public postService: PostService,
+    private route : ActivatedRoute,
+    private router: Router
+  ) { }
+
+  id:number;
+  post: Post;
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['postId'];
+    this.postService.find(this.id).subscribe((data: Post) => {
+      this.post = data;
+    })
   }
 
 }
